d9a247645d3f6d789d73f257b08298d864b04ea3
Nikita Averchenko <nikita.averchenko1@gmail.com>
2026-03-29 12:12:17 +0300

    Update FloatingText.cs

--- Assets/Scripts/Clicker/FloatingText.cs
+++ Assets/Scripts/Clicker/FloatingText.cs
@@ -14,41 +14,44 @@ namespace Clicker
         [SerializeField] private float _flyDuration = 0.5f;
         [SerializeField] private float _flyHeight = 1f;
         
+        [SerializeField] private float _appearDuration = 0.2f;
+        [SerializeField] private float _appearScale = 1f;
+        [SerializeField] private float _appearAlpha = 1f;
+        [SerializeField] private Ease _appearEase = Ease.OutBack;
+        
+        [SerializeField] private float _disappearDuration = 0.2f;
+        [SerializeField] private float _disappearScale = 0f;
+        [SerializeField] private float _disappearAlpha = 0f;
+        [SerializeField] private Ease _disappearEase = Ease.InBack;
+        
+        [SerializeField] private PathType _flightPathType = PathType.CatmullRom;
+        [SerializeField] private Ease _flightEase = Ease.InQuad;
+        
+        [SerializeField] private float _startScale = 0f;
+        [SerializeField] private float _startAlpha = 0f;
+        
         private IMemoryPool _pool;
         private Vector3 _targetPosition;
         private Sequence _animationSequence;
         
         private void Awake()
         {
-            if (_text == null)
-                _text = GetComponent<TMP_Text>();
-            
-            if (_canvasGroup == null)
-                _canvasGroup = GetComponent<CanvasGroup>();
-            
-            if (_rectTransform == null)
-                _rectTransform = GetComponent<RectTransform>();
-
+            if (_text == null) _text = GetComponent<TMP_Text>();
+            if (_canvasGroup == null) _canvasGroup = GetComponent<CanvasGroup>();
+            if (_rectTransform == null) _rectTransform = GetComponent<RectTransform>();
             gameObject.SetActive(false);
         }
         
         public void OnSpawned(float value, Vector3 targetPosition, IMemoryPool pool)
         {
             gameObject.SetActive(true);
-            
             _targetPosition = targetPosition;
             _pool = pool;
             
             _text.text = $"+{value:F0}";
             _text.color = Color.white;
-
-            transform.localScale = Vector3.zero;
-            _canvasGroup.alpha = 0f;
-
-            if (_rectTransform != null)
-            {
-                _rectTransform.anchoredPosition3D = Vector3.zero;
-            }
+            transform.localScale = Vector3.one * _startScale;
+            _canvasGroup.alpha = _startAlpha;
             
             Animate();
         }
@@ -57,9 +60,7 @@ namespace Clicker
         {
             _animationSequence?.Kill();
             _animationSequence = null;
-
             _pool = null;
-
             gameObject.SetActive(false);
         }
         
@@ -70,43 +71,14 @@ namespace Clicker
             Vector3 startPosition = transform.position;
             Vector3 middlePosition = startPosition + Vector3.up * _flyHeight;
             
-            _animationSequence.Append(transform.DOScale(1f, 0.2f).SetEase(Ease.OutBack));
-            _animationSequence.Join(_canvasGroup.DOFade(1f, 0.15f));
-            
-            _animationSequence.Append(transform.DOPath(new Vector3[] { middlePosition, _targetPosition }, 
-                _flyDuration, PathType.CatmullRom).SetEase(Ease.InQuad));
-            
-            _animationSequence.Append(transform.DOScale(0f, 0.2f).SetEase(Ease.InBack));
-            _animationSequence.Join(_canvasGroup.DOFade(0f, 0.2f));
-            
-            _animationSequence.OnComplete(() =>
-            {
-                if (_pool != null)
-                {
-                    _pool.Despawn(this);
-                }
-                else
-                {
-                    gameObject.SetActive(false);
-                }
-            });
+            _animationSequence.Append(transform.DOScale(_appearScale, _appearDuration).SetEase(_appearEase));
+            _animationSequence.Join(_canvasGroup.DOFade(_appearAlpha, _appearDuration));
+            _animationSequence.Append(transform.DOPath(new Vector3[] { middlePosition, _targetPosition }, _flyDuration, _flightPathType).SetEase(_flightEase));
+            _animationSequence.Append(transform.DOScale(_disappearScale, _disappearDuration).SetEase(_disappearEase));
+            _animationSequence.Join(_canvasGroup.DOFade(_disappearAlpha, _disappearDuration));
+            _animationSequence.OnComplete(() => _pool?.Despawn(this));
         }
 
-        public void StopAndReturn()
-        {
-            _animationSequence?.Kill();
-            if (_pool != null)
-            {
-                _pool.Despawn(this);
-            }
-            else
-            {
-                gameObject.SetActive(false);
-            }
-        }
-        
-        public class Factory : PlaceholderFactory<float, Vector3, FloatingText>
-        {
-        }
+        public class Factory : PlaceholderFactory<float, Vector3, FloatingText> { }
     }
 }
